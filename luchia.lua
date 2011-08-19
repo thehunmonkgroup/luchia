c6730aafb7baa1d78f538a6cc22ffb3f1cf41927
Chad Phillips <chad@apartmentlines.com>
2011-08-19 13:45:39 -0600

    remove testing code, make luchia bootstrap a module.

--- luchia.lua
+++ luchia.lua
@@ -5,16 +5,7 @@
 require "luchia.conf"
 require "luchia.log"
 require "luchia.server"
+require "luchia.request"
 
-luchia.log:debug("hello world")
-local server = luchia.server.Server:new()
-local params = {
-  --method = "POST",
-  path = "example/_all_docs",
-  --data = '{"hello":"world"}',
-  query_parameters = {
-    include_docs = "true",
-  },
-}
-local request = luchia.server.Request:new(server, params)
-local response = request:execute()
+module(...)
+
