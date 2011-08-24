bfff93aeef434d37a109fc7217a42cc5fa1df3d4
Chad Phillips <chad@apartmentlines.com>
2011-08-23 23:46:36 -0600

    add type, id, rev to instantiation of new document objects, fix up document object namespace.

--- luchia/document.lua
+++ luchia/document.lua
@@ -1,6 +1,7 @@
 local string = require "string"
 local log = luchia.log
 
+local type = type
 local setmetatable = setmetatable
 
 module(...)
@@ -8,7 +9,12 @@ module(...)
 function new(self, params)
   local params = params or {}
   local document = {}
-  document.data = params.data or {}
+  document._type = "document"
+  document.id = params.id
+  document.rev = params.rev
+  document.document = params.document or {}
+  document.document._id = document.id
+  document.document._rev = document.rev
   setmetatable(document, self)
   self.__index = self
   log:debug([[New document handler]])
@@ -18,13 +24,13 @@ end
 function add_attachment(self, attachment)
   local file_data = attachment:base64_encode_file(attachment.file_path)
   if file_data then
-    self.data._attachments = self.data._attachments or {}
-    self.data._attachments[attachment.file_name] = {
+    self.document._attachments = self.document._attachments or {}
+    self.document._attachments[attachment.file_name] = {
       ["content-type"] = attachment.content_type,
       data = file_data,
     }
-    log:debug(string.format([[Added attachment: %s, content_type: %s]], attachment.file_name, attachment.content_type))
-    return self.data
+    log:debug(string.format([[Added inline attachment: %s, content_type: %s]], attachment.file_name, attachment.content_type))
+    return self.document
   end
 end
 
