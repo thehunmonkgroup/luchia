3508b7844dd4c9c00216e78e851c5403b1e222e7
Chad Phillips <chad@apartmentlines.com>
2011-08-29 00:51:21 -0600

    full luadoc for configuration module.

--- luchia/conf.lua
+++ luchia/conf.lua
@@ -1,36 +1,38 @@
-module(...)
+--- Global configuration file.
+-- @author Chad Phillips
+-- @copyright 2011 Chad Phillips
+
+--- Global configuration file.
+module("luchia.conf")
 
 default = {}
 
--- DEFAULT SERVER.
+--- The default server.
+-- When no server object is created specifically, the default server is used.
+-- @field protocol The protocol to use, currently only "http" is allowed.
+-- @field host The host name, eg. "localhost" or "www.example.com".
+-- @field port The port to use, eg. "5984".
+-- @class table
+-- @name default.server
 default.server = {}
 default.server.protocol = "http"
 default.server.host = "localhost"
 default.server.port = "5984"
 
--- LOGGING OPTIONS
+--- Logging options.
 -- The lualogging package must be installed to use the logging facilities.
+-- @field appender Where to log messages. Must be one of "console", "file",
+-- false (disables logging).
+-- @field level Minimum level of messages to log. Must be one of "DEBUG",
+-- "INFO", "WARN", "ERROR", "FATAL"
+-- @field format String format for logged messages.  Tokens are %date, %level,
+-- %message, eg. "%level %message\n".
+-- @field file Full path of a file to log to if appender is set to "file", eg.
+-- "/tmp/luchia.log".
+-- @class table
+-- @name log
 log = {}
-
--- Where to log messages.  Must be one of:
---   "console"
---   "file"
---   false (disables logging)
 log.appender = "console"
-
--- Minimum level of messages to log.  Must be one of:
---   "DEBUG"
---   "INFO"
---   "WARN"
---   "ERROR"
---   "FATAL"
 log.level = "DEBUG"
-
--- String format for logged messages.  Tokens are:
---   %date
---   %level
---   %message
 log.format = "%level %message\n"
-
--- File to log to.
 log.file = "/tmp/luchia.log"
