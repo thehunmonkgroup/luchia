75dc585ed7437894aead5166f45da38148a8a9d8
Chad Phillips <chad@apartmentlines.com>
2011-09-03 11:15:44 -0400

    move log table test to setup, clean up variable namespacing.

--- luchia/tests/functional/core_log.lua
+++ luchia/tests/functional/core_log.lua
@@ -1,46 +1,39 @@
-require "luchia.core.log"
+local log = require "luchia.core.log"
 
 local tests = {}
 
-function tests.core_log_exists()
+function tests.setup()
    assert_table(luchia, "luchia")
    assert_table(luchia.core, "luchia.core")
    assert_table(luchia.core.log, "luchia.core.log")
 end
 
 function tests.test_log_log_function()
-  tests.core_log_exists()
-  assert_function(luchia.core.log.log, "luchia.core.log:log")
+  assert_function(log.log, "log:log")
 end
 
 function tests.test_log_setlevel_function()
-  tests.core_log_exists()
-  assert_function(luchia.core.log.setLevel, "luchia.core.log:setLevel")
+  assert_function(log.setLevel, "log:setLevel")
 end
 
 function tests.test_log_debug_function()
-  tests.core_log_exists()
-  assert_function(luchia.core.log.debug, "luchia.core.log:debug")
+  assert_function(log.debug, "log:debug")
 end
 
 function tests.test_log_info_function()
-  tests.core_log_exists()
-  assert_function(luchia.core.log.info, "luchia.core.log:info")
+  assert_function(log.info, "log:info")
 end
 
 function tests.test_log_warn_function()
-  tests.core_log_exists()
-  assert_function(luchia.core.log.warn, "luchia.core.log:warn")
+  assert_function(log.warn, "log:warn")
 end
 
 function tests.test_log_error_function()
-  tests.core_log_exists()
-  assert_function(luchia.core.log.error, "luchia.core.log:error")
+  assert_function(log.error, "log:error")
 end
 
 function tests.test_log_fatal_function()
-  tests.core_log_exists()
-  assert_function(luchia.core.log.fatal, "luchia.core.log:fatal")
+  assert_function(log.fatal, "log:fatal")
 end
 
 return tests
