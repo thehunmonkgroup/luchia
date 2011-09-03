edbe3744d15e3b1db340a8d5c0781ceaea343210
Chad Phillips <chad@apartmentlines.com>
2011-09-03 12:57:00 -0400

    move internal functions to locals.

--- luchia/tests/functional/core_attachment.lua
+++ luchia/tests/functional/core_attachment.lua
@@ -5,6 +5,24 @@ local tests = {}
 local text_content_type, good_text_file, good_text_default_file_name
 local good_text_custom_file_name, good_text_file_data, badfile
 
+local function get_good_file(file_name)
+  local att
+  local params = {
+    file_path = good_text_file,
+    content_type = text_content_type,
+  }
+  if file_name then
+    params.file_name = file_name
+  end
+  att = attachment:new(params)
+  return att
+end
+
+local function compare_parameter(compare_to, key, file_name)
+  local att = get_good_file(file_name)
+  assert_equal(compare_to, att[key])
+end
+
 function tests.setup()
   good_text_file, good_text_default_file_name, good_text_file_data = common.create_file1()
   text_content_type = "text/plain"
@@ -41,60 +59,42 @@ function tests.test_core_attachment_new_bad_file_path()
   assert_equal(nil, att, "Non-existent file passed.")
 end
 
-function tests.get_good_file(file_name)
-  local att
-  local params = {
-    file_path = good_text_file,
-    content_type = text_content_type,
-  }
-  if file_name then
-    params.file_name = file_name
-  end
-  att = attachment:new(params)
-  return att
-end
-
-function tests.compare_parameter(compare_to, key, file_name)
-  local att = tests.get_good_file(file_name)
-  assert_equal(compare_to, att[key])
-end
-
 function tests.test_core_attachment_new_good_file_file_path()
-  tests.compare_parameter(good_text_file, "file_path")
+  compare_parameter(good_text_file, "file_path")
 end
 
 function tests.test_core_attachment_new_good_file_content_type()
-  tests.compare_parameter(text_content_type, "content_type")
+  compare_parameter(text_content_type, "content_type")
 end
 
 function tests.test_core_attachment_new_good_file_default_file_name()
-  tests.compare_parameter(good_text_default_file_name, "file_name")
+  compare_parameter(good_text_default_file_name, "file_name")
 end
 
 function tests.test_core_attachment_new_good_file_custom_file_name()
-  tests.compare_parameter(good_text_custom_file_name, "file_name", good_text_custom_file_name)
+  compare_parameter(good_text_custom_file_name, "file_name", good_text_custom_file_name)
 end
 
 function tests.test_core_attachment_new_good_file_file_data()
-  tests.compare_parameter(good_text_file_data, "file_data")
+  compare_parameter(good_text_file_data, "file_data")
 end
 
 function tests.test_core_attachment_load_file()
-  local att = tests.get_good_file()
+  local att = get_good_file()
   local file_data = att:load_file()
   assert_equal(good_text_file_data, file_data)
 end
 
 function tests.test_core_attachment_base64_encode_file()
   require "mime"
-  local att = tests.get_good_file()
+  local att = get_good_file()
   local base64_data = att:base64_encode_file()
   assert_equal(mime.b64(good_text_file_data), base64_data)
 end
 
 function tests.test_core_attachment_prepare_request()
   local server = {}
-  local att = tests.get_good_file()
+  local att = get_good_file()
   att:prepare_request(server)
   assert_equal(att.content_type, server.content_type)
   assert_equal(att.file_data, server.request_data)
