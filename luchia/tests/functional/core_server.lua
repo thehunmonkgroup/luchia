00a1e44be5b58c3656d77abbfd5fc11af999bf1d
Chad Phillips <chad@apartmentlines.com>
2011-09-05 14:20:10 -0400

    add function to build custom request server. add tests for prepare_request and parse_json. rename custom request helper function.

--- luchia/tests/functional/core_server.lua
+++ luchia/tests/functional/core_server.lua
@@ -13,7 +13,12 @@ local bad_port_low = "0"
 local bad_port_nan = "foo"
 local user = "user"
 local password = "password"
-local custom_request_function = function() end
+
+local content_type = "application/json"
+local json_good = '{"foo":"bar"}'
+local json_good_key = "foo"
+local json_good_value = "bar"
+local json_bad = 'foo'
 
 local function valid_server_table(srv)
   assert_table(srv, "srv")
@@ -29,6 +34,22 @@ local function bad_server_param(protocol, host, port)
   }
   local srv = server:new(params)
   assert_equal(nil, srv, "srv")
+end
+
+local function request_function(request)
+end
+
+local function custom_request_server(params)
+  params = params or {}
+  params.custom_request_function = request_function
+  local srv = server:new(params)
+  valid_server_table(srv)
+  return srv
+end
+
+local function prepare_request(self, server)
+  server.content_type = content_type
+  server.request_data = json_good
 end
 
 function tests.setup()
@@ -77,7 +98,7 @@ function tests.test_core_server_new_all_params()
     port = good_port,
     user = user,
     password = password,
-    custom_request_function = custom_request_function,
+    custom_request_function = request_function,
   }
   local srv = server:new(params)
   valid_server_table(srv)
@@ -87,7 +108,52 @@ function tests.test_core_server_new_all_params()
   assert_equal(user, srv.connection.user, "srv.connection.user")
   assert_equal(password, srv.connection.password, "srv.connection.password")
   assert_equal(5, common.table_length(srv.connection), "srv.connection length")
-  assert_equal(custom_request_function, srv.request_function, "custom srv.request_function")
+  assert_equal(request_function, srv.request_function, "custom srv.request_function")
+end
+
+function tests.test_core_server_prepare_request_reset_data()
+  local srv = custom_request_server()
+  srv.content_type = content_type
+  srv.request_data = request_data
+  srv:prepare_request()
+  assert_equal(nil, srv.content_type, "srv.content_type")
+  assert_equal(nil, srv.request_data, "srv.request_data")
+end
+
+function tests.test_core_server_prepare_request_set_data()
+  local srv = custom_request_server()
+  srv.data = {
+    prepare_request = prepare_request,
+  }
+  srv:prepare_request()
+  assert_equal(content_type, srv.content_type, "srv.content_type")
+  assert_equal(json_good, srv.request_data, "srv.request_data")
+end
+
+function tests.test_core_server_parse_json_nil()
+  local srv = custom_request_server()
+  local result = srv:parse_json()
+  assert_equal(nil, result, "result")
+end
+
+function tests.test_core_server_parse_json_empty()
+  local srv = custom_request_server()
+  local result = srv:parse_json("")
+  assert_equal(nil, result, "result")
+end
+
+function tests.test_core_server_parse_json_bad()
+  local srv = custom_request_server()
+  local result = srv:parse_json(json_bad)
+  assert_false(result, "result")
+end
+
+function tests.test_core_server_parse_json_good()
+  local srv = custom_request_server()
+  local result = srv:parse_json(json_good)
+  assert_table(result, "result")
+  assert_equal(result[json_good_key], json_good_value, "result")
+  assert_equal(1, common.table_length(result), "result")
 end
 
 return tests
