d2575f6d5b7bd0bfb65d6009bd9dbd7f1a7095d9
Chad Phillips <chad@apartmentlines.com>
2011-09-06 14:13:28 -0400

    break out prepare_request test for core attachements. isolate creation of temporary files to only tests that need them. reduce new core attachment tests to only two cases.

--- luchia/tests/functional/core_attachment.lua
+++ luchia/tests/functional/core_attachment.lua
@@ -2,36 +2,23 @@ local common = require "luchia.tests.common"
 local attachment = require "luchia.core.attachment"
 
 local tests = {}
-local text_content_type, good_text_file, good_text_default_file_name
-local good_text_custom_file_name, good_text_file_data, badfile
 
-local function get_good_file(file_name)
-  local att
+local text_content_type = "text/plain"
+local custom_file_name = "custom_textfile1.txt"
+local badfile = "badfile.txt"
+
+local function build_new_attachment(file_name)
+  local file_path, default_file_name, file_data = common.create_file1()
   local params = {
-    file_path = good_text_file,
+    file_path = file_path,
     content_type = text_content_type,
   }
   if file_name then
     params.file_name = file_name
   end
-  att = attachment:new(params)
-  return att
-end
-
-local function compare_parameter(compare_to, key, file_name)
-  local att = get_good_file(file_name)
-  assert_equal(compare_to, att[key])
-end
-
-function tests.setup()
-  good_text_file, good_text_default_file_name, good_text_file_data = common.create_file1()
-  text_content_type = "text/plain"
-  good_text_custom_file_name = "custom_textfile1.txt"
-  badfile = "badfile.txt"
-end
-
-function tests.teardown()
+  local att = attachment:new(params)
   common.remove_file1()
+  return att, file_path, default_file_name, file_data
 end
 
 function tests.test_core_attachment_new_no_params()
@@ -59,44 +46,52 @@ function tests.test_core_attachment_new_bad_file_path()
   assert_equal(nil, att, "Non-existent file passed.")
 end
 
-function tests.test_core_attachment_new_good_file_file_path()
-  compare_parameter(good_text_file, "file_path")
+function tests.test_core_attachment_new_good_params()
+  local att, file_path, default_file_name, file_data = build_new_attachment()
+  assert_equal(file_path, att.file_path, "file_path")
+  assert_equal(text_content_type, att.content_type, "content_type")
+  assert_equal(default_file_name, att.file_name, "default file_name")
+  assert_equal(file_data, att.file_data, "file_data")
 end
 
-function tests.test_core_attachment_new_good_file_content_type()
-  compare_parameter(text_content_type, "content_type")
-end
-
-function tests.test_core_attachment_new_good_file_default_file_name()
-  compare_parameter(good_text_default_file_name, "file_name")
-end
-
-function tests.test_core_attachment_new_good_file_custom_file_name()
-  compare_parameter(good_text_custom_file_name, "file_name", good_text_custom_file_name)
-end
-
-function tests.test_core_attachment_new_good_file_file_data()
-  compare_parameter(good_text_file_data, "file_data")
+function tests.test_core_attachment_new_good_params_custom_file_name()
+  local att, file_path, default_file_name, file_data = build_new_attachment(custom_file_name)
+  assert_equal(file_path, att.file_path, "file_path")
+  assert_equal(text_content_type, att.content_type, "content_type")
+  assert_equal(custom_file_name, att.file_name, "custom file_name")
+  assert_equal(file_data, att.file_data, "file_data")
 end
 
 function tests.test_core_attachment_load_file()
-  local att = get_good_file()
-  local file_data = att:load_file()
-  assert_equal(good_text_file_data, file_data)
+  local file_path, default_file_name, file_data = common.create_file1()
+  local params = {
+    file_path = file_path,
+    content_type = text_content_type,
+  }
+  local att = attachment:new(params)
+  local loaded_file_data = att:load_file()
+  common.remove_file1()
+  assert_equal(file_data, loaded_file_data, "loaded file_data")
 end
 
 function tests.test_core_attachment_base64_encode_file()
   require "mime"
-  local att = get_good_file()
+  local att, file_path, default_file_name, file_data = build_new_attachment()
   local base64_data = att:base64_encode_file()
-  assert_equal(mime.b64(good_text_file_data), base64_data)
+  assert_equal(mime.b64(file_data), base64_data)
 end
 
-function tests.test_core_attachment_prepare_request()
+function tests.test_core_attachment_prepare_request_content_type()
   local server = {}
-  local att = get_good_file()
+  local att = build_new_attachment()
   att:prepare_request(server)
   assert_equal(att.content_type, server.content_type)
+end
+
+function tests.test_core_attachment_prepare_request_request_data()
+  local server = {}
+  local att = build_new_attachment()
+  att:prepare_request(server)
   assert_equal(att.file_data, server.request_data)
 end
 
