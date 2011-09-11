f44c25a5b7524a32069209a622e46d28a65a4a46
Chad Phillips <chad@apartmentlines.com>
2011-09-11 10:30:06 -0400

    move test suite loading for database, document, and utilities classes to integration suite.

--- luchia/tests/run_integration_tests.lua
+++ luchia/tests/run_integration_tests.lua
@@ -4,6 +4,9 @@ require "lunatest"
 require "luchia.tests.common"
 
 lunatest.suite("luchia.tests.integration.conf")
+lunatest.suite("luchia.tests.integration.database")
+lunatest.suite("luchia.tests.integration.document")
+lunatest.suite("luchia.tests.integration.utilities")
 
 lunatest.run()
 
--- luchia/tests/run_unit_tests.lua
+++ luchia/tests/run_unit_tests.lua
@@ -7,8 +7,5 @@ lunatest.suite("luchia.tests.unit.core_log")
 lunatest.suite("luchia.tests.unit.core_server")
 lunatest.suite("luchia.tests.unit.core_attachment")
 lunatest.suite("luchia.tests.unit.core_document")
-lunatest.suite("luchia.tests.unit.database")
-lunatest.suite("luchia.tests.unit.document")
-lunatest.suite("luchia.tests.unit.utilities")
 
 lunatest.run()
