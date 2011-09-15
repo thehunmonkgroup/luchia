c636e9d34815c7ba37c98ce845ec8d8470db8242
Chad Phillips <chad@apartmentlines.com>
2011-09-15 17:03:03 -0400

    better name for document integration test.

--- tests/integration/document.lua
+++ tests/integration/document.lua
@@ -34,7 +34,7 @@ function tests.teardown()
   assert_true(db:response_ok(resp), "Unable to delete database")
 end
 
-function tests.test_new_document_returns_nil()
+function tests.test_new_document_no_database_returns_nil()
   local bad_doc = document:new()
   assert_equal(nil, bad_doc, "Document should not have been created without database name")
 end
