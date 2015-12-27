e5d9cb7106184bc5a9f1c0ec01b3e75c0c89025b
Chad Phillips <chad@apartmentlines.com>
2015-12-28 05:53:49 +1100

    unit test coverage for high-level database

--- tests/unit/high_level_database.lua
+++ tests/unit/high_level_database.lua
@@ -6,19 +6,20 @@ local assert_table = lunatest.assert_table
 local common = require "common_test_functions"
 local database = require "luchia.database"
 
-local good_protocol = "http"
-local good_host = "www.example.com"
-local good_port = "5984"
-local user = "user"
-local password = "password"
+local good_protocol = common.server_good_protocol
+local good_host = common.server_good_host
+local good_port = common.server_good_port
+local user = common.server_user
+local password = common.server_password
+local request_function = common.server_request
+local example_database = common.server_example_database
 local conf = {
   default = {
     server = {
       protocol = good_protocol,
       host = good_host,
       port = good_port,
-      user = user,
-      password = password,
+      custom_request_function = request_function,
     },
   },
 }
@@ -60,6 +61,65 @@ end
 function tests.test_new_with_custom_server_params_returns_only_server()
   local db = new_with_custom_server_params()
   assert_equal(1, common.table_length(db), "db length")
+end
+
+function tests.test_list_returns_valid_list()
+  local db = new_with_custom_server_params()
+  local response, response_code, headers, status = db:list()
+  assert_table(response)
+end
+
+function tests.test_info_with_database_name_returns_valid_database_info()
+  local db = new_with_custom_server_params()
+  local response, response_code, headers, status = db:info(example_database)
+  assert_table(response)
+  assert_equal(response.db_name, example_database)
+end
+
+function tests.test_info_with_no_database_name_returns_nil()
+  local db = new_with_custom_server_params()
+  local response, response_code, headers, status = db:info()
+  assert_equal(response, nil)
+end
+
+function tests.test_create_with_database_name_returns_valid_response()
+  local db = new_with_custom_server_params()
+  local response, response_code, headers, status = db:create(example_database)
+  assert_table(response)
+  assert_equal(response.ok, true)
+end
+
+function tests.test_create_with_no_database_name_returns_nil()
+  local db = new_with_custom_server_params()
+  local response, response_code, headers, status = db:create()
+  assert_equal(response, nil)
+end
+
+function tests.test_delete_with_database_name_returns_valid_response()
+  local db = new_with_custom_server_params()
+  local response, response_code, headers, status = db:delete(example_database)
+  assert_table(response)
+  assert_equal(response.ok, true)
+end
+
+function tests.test_delete_with_no_database_name_returns_nil()
+  local db = new_with_custom_server_params()
+  local response, response_code, headers, status = db:delete()
+  assert_equal(response, nil)
+end
+
+function tests.test_response_ok_with_ok_response_returns_true()
+  local db = new_with_custom_server_params()
+  local response = {ok = true}
+  local bool = db:response_ok(response)
+  assert_equal(bool, true)
+end
+
+function tests.test_response_ok_with_not_ok_response_returns_false()
+  local db = new_with_custom_server_params()
+  local response = {ok = false}
+  local bool = db:response_ok(response)
+  assert_equal(bool, false)
 end
 
 return tests
