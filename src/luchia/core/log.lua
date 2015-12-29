2f904e5ac52743bc31f9bfe3b995f9c5b48c9c24
Chad Phillips <chad@apartmentlines.com>
2015-12-29 00:06:26 -0500

    update doc for logger.

--- src/luchia/core/log.lua
+++ src/luchia/core/log.lua
@@ -2,6 +2,17 @@
 -- @author Chad Phillips
 -- @copyright 2011 Chad Phillips
 
+--- Core logging class.
+-- See the method documentation for more detail, here is a quick primer:</p>
+-- <p><code>
+-- -- Require the class.<br />
+-- local logger = require "luchia.core.logger"<br />
+-- -- Set up a shortcut to the logger function.<br />
+-- local log = logger.logger<br />
+-- -- Log something.<br />
+-- log:info("Hello world!")<br />
+-- </p></code>
+
 --- Log a message at the debug level.
 -- @param self
 -- @param message
@@ -10,7 +21,7 @@
 --   that of string.format() can be used, eg. <code>log:debug("hello world")
 --   </code> and <code>log:debug("hello %s", "world")</code> are both valid
 --   and will produce the same output.
--- @usage luchia.core.log:debug("debug message")
+-- @usage luchia.core.log.logger:debug("debug message")
 -- @class function
 -- @name debug
 
@@ -18,7 +29,7 @@
 -- @param self
 -- @param message
 --   The message to log.
--- @usage luchia.core.log:info("info message")
+-- @usage luchia.core.log.logger:info("info message")
 -- @class function
 -- @name info
 
@@ -26,7 +37,7 @@
 -- @param self
 -- @param message
 --   The message to log.
--- @usage luchia.core.log:warn("warn message")
+-- @usage luchia.core.log.logger:warn("warn message")
 -- @class function
 -- @name warn
 
@@ -34,7 +45,7 @@
 -- @param self
 -- @param message
 --   The message to log.
--- @usage luchia.core.log:error("error message")
+-- @usage luchia.core.log.logger:error("error message")
 -- @class function
 -- @name error
 
@@ -42,7 +53,7 @@
 -- @param self
 -- @param message
 --   The message to log.
--- @usage luchia.core.log:fatal("fatal message")
+-- @usage luchia.core.log.logger:fatal("fatal message")
 -- @class function
 -- @name fatal
 
@@ -51,6 +62,13 @@ local conf = require "luchia.conf"
 
 local _M = {}
 
+--- Reconfigure the logger.
+-- @param self
+-- @param config
+--   Configuration settings for the logger.
+-- @usage luchia.core.logger:set_logger(config)
+-- @class function
+-- @see luchia.conf
 _M.set_logger = function(self, config)
   config = config or conf.log
   if config.appender == "file" then
